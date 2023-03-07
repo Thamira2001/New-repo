f29476dbd27579292fc4618baf3e6fa02ee233d4
Brett Aseltine <aseltinb@myumanitoba.ca>
2023-03-07 17:13:01 +0000

    Brett branch

--- app/src/main/java/comp3350/srsys/business/Statistics.java
+++ app/src/main/java/comp3350/srsys/business/Statistics.java
@@ -1,5 +1,7 @@
 package comp3350.srsys.business;
 
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.srsys.objects.Workout;
@@ -19,8 +21,19 @@ public class Statistics {
         int durationSec = 0;
         for(int i = 0; i < workoutList.size(); i++) {
             Workout w = workoutList.get(i);
-            durationSec += (int)(w.getEndTimeSec() - w.getStartTimeSec());
+            durationSec += w.getDurationSec();
         }
         return durationSec/workoutList.size()/60;
+    }
+
+    public int[] workoutMinutesByMonth() {
+        int[] monthData = new int[12];
+        for(int i = 0; i < workoutList.size(); i++) {
+            Workout w = workoutList.get(i);
+            Month m = w.getMonth();
+            int durationMinutes = w.getDurationSec()/60;
+            monthData[m.getValue()] += durationMinutes;
+        }
+        return monthData;
     }
 }
